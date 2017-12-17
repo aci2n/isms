482e0262deada93e061d5a918b3229ecbbff273f
Alvaro Calace <alvarocalace@hotmail.com>
2017-12-17 20:34:08 -0300

    Add "location" component placeholder

---
+++ Web/app/modules/dashboard/location/location.component.js
@@ -0,0 +1,19 @@
+(function () {
+	'use strict';
+	
+	class LocationController {
+
+	}
+	LocationController.$inject = [];
+
+	const component = {
+        templateUrl: 'dashboard/location/location.html',
+        controller: LocationController,
+        controllerAs: 'locationVm',
+        bindings: {
+            sections: '<'
+        }
+    };
+
+	angular.module('isms.dashboard').component('location', component);
+}());
